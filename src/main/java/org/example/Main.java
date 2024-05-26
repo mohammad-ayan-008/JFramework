9e5004b8aba154d7b9de9a3e3fde4682b3b2ba0e
mohammad-ayan-008 <mohammadayanafaq@gmail.com>
2024-05-26 17:06:15 +0530

    major changes and  bug fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,6 @@ public class Main {
     public static testInterface infs;
 
     public static void main(String[] args) {
-
          new JDBCInitilaizer("org.example");
        //  System.out.println(infs.save(new Client(2,"AYan")));
 //         System.out.println(infs.findAll(Client.class));
